39de64a2a8f5e5d42a4558f98768e6a44b8ffb29
Adam Mason <adam.mason86@gmail.com>
2017-03-12 18:19:36 -0400

    beautify

--- js/controller/ctrl.js
+++ js/controller/ctrl.js
@@ -1,23 +1,23 @@
 var app = angular.module('mod');
 
-app.controller('ctrl', function($scope, $interval){
-  var date = new Date();
-  $scope.year = date.getFullYear();
+app.controller('ctrl', function($scope, $interval) {
+    var date = new Date();
+    $scope.year = date.getFullYear();
 
-  var luigi = [
-  "img/walk1.png",
-  "img/walk2.png",
-  "img/walk3.png"
-  ];
-  var walkIndex = 0
-  $scope.walkPics = luigi;
-  $scope.luigi = $scope.walkPics[walkIndex];
+    var luigi = [
+        "img/walk1.png",
+        "img/walk2.png",
+        "img/walk3.png"
+    ];
+    var walkIndex = 0
+    $scope.walkPics = luigi;
+    $scope.luigi = $scope.walkPics[walkIndex];
 
-    $interval(function(){
+    $interval(function() {
 
-      if(walkIndex>2){
-        walkIndex=0;
-      };
-      $scope.luigi = $scope.walkPics[walkIndex++];
-      }, 100);
+        if (walkIndex > 2) {
+            walkIndex = 0;
+        };
+        $scope.luigi = $scope.walkPics[walkIndex++];
+    }, 100);
 });
--- js/module/module.js
+++ js/module/module.js
@@ -1,23 +1,25 @@
 var app = angular.module('mod', ['ngRoute']);
 
-app.config(function($routeProvider, $locationProvider){
-  $routeProvider
-    .when('/about', {
-      controller: 'ctrl',
-      templateUrl: 'views/about.html'
-    })
-    .when('/work', {
-      controller: 'ctrl',
-      templateUrl: 'views/work.html'
-    })
-    .when('/contact', {
-      controller: 'ctrl',
-      templateUrl: 'views/contact.html'
-    })
-    .otherwise({redirectTo: '/about'});
-  $locationProvider.hashPrefix('');
-}).directive('break', function(){
-  return {
-    template: '<div class="break"><div class="container"><div class="row"><div class="col-md-12"><p>&#60;&#47;&#62;</p></div></div></div></div>'
-  };
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/about', {
+            controller: 'ctrl',
+            templateUrl: 'views/about.html'
+        })
+        .when('/work', {
+            controller: 'ctrl',
+            templateUrl: 'views/work.html'
+        })
+        .when('/contact', {
+            controller: 'ctrl',
+            templateUrl: 'views/contact.html'
+        })
+        .otherwise({
+            redirectTo: '/about'
+        });
+    $locationProvider.hashPrefix('');
+}).directive('break', function() {
+    return {
+        template: '<div class="break"><div class="container"><div class="row"><div class="col-md-12"><p>&#60;&#47;&#62;</p></div></div></div></div>'
+    };
 });
